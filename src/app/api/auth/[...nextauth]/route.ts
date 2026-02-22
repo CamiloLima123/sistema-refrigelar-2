679f325a4fa61658c3eed443b52fb93b87bf8855
CamiloLima123 <celulardocamilo@gmail.com>
2026-02-22 07:48:12 -0300

    Update route.ts

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,1 +1,33 @@
+import NextAuth from "next-auth"
+import CredentialsProvider from "next-auth/providers/credentials"
 
+const handler = NextAuth({
+  providers: [
+    CredentialsProvider({
+      name: "credentials",
+      credentials: {
+        email: { label: "Email", type: "text" },
+        password: { label: "Password", type: "password" }
+      },
+
+      async authorize(credentials) {
+        // usuário TEMPORÁRIO para teste
+        if (
+          credentials?.email === "admin@email.com" &&
+          credentials?.password === "123456"
+        ) {
+          return {
+            id: "1",
+            name: "Administrador",
+            email: "admin@email.com"
+          }
+        }
+
+        return null
+      }
+    })
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+})
+
+export { handler as GET, handler as POST }
